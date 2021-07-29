13175214271ca8a2608e33a8b572d22cec2bc609
Makizushi3510max <mikoto.makizuru3510@gmail.com>
2021-07-30 01:28:55 +0900

    undo

--- covids/covid.js
+++ covids/covid.js
@@ -10,16 +10,10 @@ const client = new line.Client({
 const clovaSkillHandler = clova.Client
   .configureSkill()
   .onLaunchRequest(responseHelper => {
-    const json = JSON.parse(fs.readFileSync('./daily_positive_detail.json', 'utf8'));
-    const latest_data = json.data.slice(-1)[0];
-    const date = new Date(latest_data.diagnosed_date);
-
     responseHelper.setSimpleSpeech(
-        SpeechBuilder.createSpeechText(`東京都の${date.getMonth()+1}月${date.getDate()}日の感染者数は${latest_data.count}人です。`)
+      clova.SpeechBuilder.createSpeechText('ご注文は何にしましょうか？')
     );
-    responseHelper.endSession();
   })
-
 // スキルが終了するときに呼び出されます
 
 module.exports = clovaSkillHandler;
