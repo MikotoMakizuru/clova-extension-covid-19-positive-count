909f6397ab36e6b68b177417b31587abf9a51bc0
Makizushi3510max <mikoto.makizuru3510@gmail.com>
2021-07-29 23:01:47 +0900

    First commit

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    PORT: process.env.PORT || 3000,
+    DEBUG: process.env.DEBUG || false,
+    APPLICATION_ID: "io.mikoto.github.covid19",
+    CHANNEL_ACCESS_TOKEN: "YOUR_CHANNEL_ACCESS_TOKEN",
+  }
+  
