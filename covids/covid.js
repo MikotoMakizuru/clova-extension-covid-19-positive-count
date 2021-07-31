7c6fe825412e32fc2193dbd3643c023ab22a7886
Makizushi3510max <mikoto.makizuru3510@gmail.com>
2021-07-31 15:52:49 +0900

    messenger_aip_comment_out

--- covids/covid.js
+++ covids/covid.js
@@ -1,11 +1,11 @@
 const clova = require('@line/clova-cek-sdk-nodejs');
 const { CHANNEL_ACCESS_TOKEN } = require('../config.js');
 
-// LINEメッセンジャーAPI
-const line = require('@line/bot-sdk');
-const client = new line.Client({
-  channelAccessToken: CHANNEL_ACCESS_TOKEN
-})
+//LINEメッセンジャーAPI
+//const line = require('@line/bot-sdk');
+//const client = new line.Client({
+  //channelAccessToken: CHANNEL_ACCESS_TOKEN
+//})
 
 const clovaSkillHandler = clova.Client
   .configureSkill()
